7477ac65effb54ed2d02ea591ba529a31b25a84d
ian <iandavis6481@gmail.com>
2022-12-26 14:52:24 -0600

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const prisma = new PrismaClient();
 const app = express();
 
